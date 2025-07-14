8a05cf1bd19dd218261101157268fbbc5f7568e2
Amitish <58432032+Amitish@users.noreply.github.com>
2025-07-14 21:43:27 +0530

    pehle branch (#1)
    Co-authored-by: Amit Bisht <amit.bisht@dishtv.in>

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "pikka" {
   name     = "pikka-chu"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "Ash" {
+  name     = "Ash"
+  location = "West Europe"
+}
