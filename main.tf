820036855d9d637b3de068051f018d7fdbffaa8b
Amit Bisht <amit.bisht@dishtv.in>
2025-07-14 23:23:25 +0530

    ho ja bhai

--- main.tf
+++ main.tf
@@ -7,13 +7,3 @@ resource "azurerm_resource_group" "Ash" {
   name     = "Ash"
   location = "West Europe"
 }
-
-resource "azurerm_resource_group" "Misty" {
-  name     = "Misty"
-  location = "West Europe"
-}
-
-resource "azurerm_resource_group" "Pokemon" {
-  name     = "Pokemon"
-  location = "West Europe"
-}
